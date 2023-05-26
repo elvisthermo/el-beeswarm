3c6a8a27a21161344a78ddc35eb3b55c955e5be5
elvisthermo <elvis.thermo@gmail.com>
2023-05-25 21:05:25 -0300

    add configs and new dependencies

--- sonar-project.js
+++ sonar-project.js
@@ -1,15 +1,16 @@
-import sonarqubeScanner from "sonarqube-scanner";
+import sonarqubeScanner from 'sonarqube-scanner';
 
 sonarqubeScanner(
   {
-    serverUrl: "http://localhost:9000",
+    serverUrl: 'http://localhost:9000',
     options: {
-      "sonar.sources": "src",
-      "sonar.exclusions": "src/**.spec.ts, coverager", 
+      'sonar.sources': 'src',
+      'sonar.exclusions': 'src/**.spec.ts, coverager',
       'sonar.tests': 'src',
-      'sonar.test.inclusions':  'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
-            'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
+      'sonar.test.inclusions':
+        'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
+      'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
     },
   },
-  () => process.exit()
+  () => process.exit(),
 );
